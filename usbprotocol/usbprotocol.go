629af51f7182ad8a803aef7bd35a16fa57d613fd
spritkopf <spritkopf@gmail.com>
2021-02-07 15:19:58 +0100

    usbprotocol: sending data working

--- usbprotocol/usbprotocol.go
+++ usbprotocol/usbprotocol.go
@@ -3,9 +3,39 @@ package usbprotocol
 import (
 	"fmt"
 
+	"github.com/sigurn/crc16"
 	"go.bug.st/serial.v1"
 )
 
+// SizeError is returned, when input data is of invalid size (e.g. message payload for transfer)
+type SizeError uint32
+
+func (f SizeError) Error() string {
+	return fmt.Sprintf("Size of passed data too large, allowed %v, got %v", MaxPayloadLen, uint32(f))
+}
+
+// MaxPayloadLen - maximum length of message Payload (64 byte packet - 4 bytes header - 2 bytes crc)
+const MaxPayloadLen = 64 - 4 - 2
+
+// sync byte, marks the beginning of a new packet
+const sync = 0x69
+
+const (
+	// CmdVersion - Get firmware version
+	CmdVersion = 0x10
+	// CmdTransfer - Send a message, wait for reply
+	CmdTransfer = 0x30
+	// CmdSend - Send a message without reply
+	CmdSend = 0x31
+	// CmdTest - test command, do not use
+	CmdTest = 0x61
+	// CmdIrq - interrupt callback, only from device to host
+	CmdIrq = 0x80
+	// CmdRx - callback from incoming ESB message
+	CmdRx = 0x81
+)
+
+var crcTable *crc16.Table
 var port serial.Port
 
 // Open connects to the specified virtual COM port
@@ -27,11 +57,37 @@ func Close() {
 }
 
 // Transfer sends a message to the usb device and returns the answer
-func Transfer(message []byte) {
+func Transfer(message []byte) ([]byte, error) {
+	if len(message) > MaxPayloadLen {
+		return nil, SizeError(len(message))
+	}
+
 	fmt.Printf("%s", message)
+
+	var txBuf [64]byte
+	txBuf[0] = sync
+	txBuf[1] = CmdTest
+	txBuf[2] = 0
+	txBuf[3] = byte(len(message))
+	copy(txBuf[4:], message[:])
+	crc := crc16.Checksum(txBuf[:len(txBuf)-2], crcTable)
+	var h, l uint8 = uint8(crc & 0xff), uint8(crc >> 8)
+	txBuf[62] = byte(h)
+	txBuf[63] = byte(l)
+
+	bytesWritten, err := port.Write(txBuf[:])
+
+	fmt.Println(bytesWritten)
+	fmt.Println(err)
+	// if err != nil || bytesWritten != len(txBuf) {
+	// 	return nil, err
+	// }
+
+	return nil, nil
 }
 
 func init() {
-	// Todo: do init stuff here
-	fmt.Printf("Init")
+	// create crc16 table
+	crcTable = crc16.MakeTable(crc16.CRC16_CCITT_FALSE)
+	//fmt.Printf("Init")
 }
--- usbprotocol/usbprotocol_test.go
+++ usbprotocol/usbprotocol_test.go
@@ -4,16 +4,9 @@ import (
 	"testing"
 )
 
-// Test
-func TestHelloName(t *testing.T) {
-	a := []byte{'g', 'o', 'a', 'l'}
-
-	Transfer(a)
-
-}
-
+//TestOpenSuccess tests that the virtual COM port can be opened
 func TestOpenSuccess(t *testing.T) {
-	err := Open(("/dev/ttyACM0a"))
+	err := Open(("/dev/ttyACM0"))
 
 	if err != nil {
 		t.Fatal(err)
@@ -22,10 +15,41 @@ func TestOpenSuccess(t *testing.T) {
 	Close()
 }
 
+//TestOpenFail tests the error handling of Open() in case of bad port name
 func TestOpenFail(t *testing.T) {
 	err := Open(("djkdskfj"))
 
 	if err == nil {
 		t.FailNow()
 	}
+}
+
+// TestTransfer tests the successful operation of the Transfer function
+func TestTransfer(t *testing.T) {
+
+	Open("/dev/ttyACM0")
+
+	msg := []byte{1, 2, 3, 4}
+	_, err := Transfer(msg)
+
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	Close()
+
+}
+
+// TestTransferMessageTooLong tests the error handling of the Transfer function
+func TestTransferMessageTooLong(t *testing.T) {
+
+	msg := make([]byte, 64)
+	//msg := []byte{'g', 'o', 'a', 'l'}
+	_, err := Transfer(msg)
+
+	_, ok := err.(SizeError)
+	if !ok {
+		t.Fatalf("Expected Transfer to fail because of too large message parameter")
+	}
+
 }
